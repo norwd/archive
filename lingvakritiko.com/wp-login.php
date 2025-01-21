84410f9a5974165d5a8de03a7b7a45aca6de986e
norwd <106889957+norwd@users.noreply.github.com>
2025-01-21 03:04:28 +0000

    Download lingvakritiko.com/
    Original source: https://lingvakritiko.com/
    
    **NOTE:** This is an automatic commit. See the archive workflow.
    
    Signed-off-by: norwd <106889957+norwd@users.noreply.github.com>

---
+++ lingvakritiko.com/wp-login.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+	<html lang="eo">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Perdita pasvorto &lsaquo; Lingva Kritiko &#8212; WordPress</title>
+	<meta name='robots' content='max-image-preview:large, noindex, noarchive' />
+<link rel='stylesheet' id='dashicons-css' href='wp-includes/css/dashicons.min.css?ver=6.4.2' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css' href='wp-includes/css/buttons.min.css?ver=6.4.2' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css' href='wp-admin/css/forms.min.css?ver=6.4.2' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css' href='wp-admin/css/l10n.min.css?ver=6.4.2' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css' href='wp-admin/css/login.min.css?ver=6.4.2' type='text/css' media='all' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+		</head>
+	<body class="login no-js login-action-lostpassword wp-core-ui  locale-eo">
+	<script type="text/javascript">
+/* <![CDATA[ */
+document.body.className = document.body.className.replace('no-js','js');
+/* ]]> */
+</script>
+
+		<div id="login">
+		<h1><a href="https://wordpress.org/">Povigita de WordPress</a></h1>
+	<div class="notice notice-info message"><p>Entajpu viajn uzantnomo kaj retpoŝtadreson. Vi ricevos retmesaĝon kun instrukcioj pri kiel restarigi vian pasvorton.</p></div>
+
+		<form name="lostpasswordform" id="lostpasswordform" action="wp-login.php?action=lostpassword" method="post">
+			<p>
+				<label for="user_login">Uzantnomo aŭ retpoŝtadreso</label>
+				<input type="text" name="user_login" id="user_login" class="input" value="" size="20" autocapitalize="off" autocomplete="username" required="required" />
+			</p>
+						<input type="hidden" name="redirect_to" value="" />
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Havigi novan pasvorton" />
+			</p>
+		</form>
+
+		<p id="nav">
+			<a class="wp-login-log-in" href="wp-login.php">Ensaluti</a>
+					</p>
+				<p id="backtoblog">
+			<a href="index.html">&larr; Iri al Lingva Kritiko</a>		</p>
+			</div>
+				<div class="language-switcher">
+				<form id="language-switcher" action="wp-login.php?action=lostpassword" method="get">
+
+					<label for="language-switcher-locales">
+						<span class="dashicons dashicons-translation" aria-hidden="true"></span>
+						<span class="screen-reader-text">
+							Lingvo						</span>
+					</label>
+
+					<select name="wp_lang" id="language-switcher-locales"><option value="en_US" lang="en" data-installed="1">English (United States)</option>
+<option value="eo" lang="eo" selected='selected' data-installed="1">Esperanto</option></select>
+					
+					
+											<input type="hidden" name="action" value="lostpassword" />
+					
+						<input type="submit" class="button" value="Ŝanĝi">
+
+					</form>
+				</div>
+				<script type="text/javascript">
+/* <![CDATA[ */
+
+		try{document.getElementById('user_login').focus();}catch(e){}
+		if(typeof wpOnload==='function')wpOnload();
+		
+/* ]]> */
+</script>
+	</body>
+	</html>
+	
